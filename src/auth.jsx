bc993bde0c0666891aa10ef2e257d2e6961a19f2
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-16 08:07:14 +0530

    Update: requests access token after it expire

--- src/auth.jsx
+++ src/auth.jsx
@@ -13,11 +13,27 @@ const accessToken = async () => {
       Authorization: `Basic ${authHeader}`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: formData
+    body: formData,
   };
-  const body = await fetch("https://accounts.spotify.com/api/token", payLoad);
-  const response = await body.json();
+
+  let body;
+  let response;
+  const expireAt = localStorage.getItem("expireAt");
+
+  if (Date.now() < +expireAt) {
+    body = await fetch("https://accounts.spotify.com/api/token", payLoad);
+    response = await body.json();
+    localStorage.setItem("access_token", response.access_token);
+    localStorage.setItem("expireAt", Date.now() + response.expires_in);
+  } else {
+    response = localStorage.getItem("access_token");
+  }
   console.log(response);
 };
 
 export default accessToken;
+// {
+//     "access_token": "BQANYvOmsxjq_r9eQmGR7wEGJHmmdnIzGpN301LfYdlt04169xpsQ-gzvCp0jsrqXsQXOsg0coZEyMJLjU-YiZX3yLg2MZmOnWi2ggJXZh5Kzb-X6FJ-dAiyEl3GAUAc0D4rJ1IqTXc",
+//     "token_type": "Bearer",
+//     "expires_in": 3600
+// }

1062d2bff66766515025e375ae11ccc280aec74b
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-16 08:09:32 +0530

    Update: replace input tag with SearchResult component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
-import './App.css'
+import accessToken from "./auth.jsx";
+import { SearchResult } from "./components/containers/SearchResult.jsx";
+
+// {
+//     "access_token": "BQANYvOmsxjq_r9eQmGR7wEGJHmmdnIzGpN301LfYdlt04169xpsQ-gzvCp0jsrqXsQXOsg0coZEyMJLjU-YiZX3yLg2MZmOnWi2ggJXZh5Kzb-X6FJ-dAiyEl3GAUAc0D4rJ1IqTXc",
+//     "token_type": "Bearer",
+//     "expires_in": 3600
+// }
 
 function App() {
-  
+  accessToken()
 
-  return 
+  return (
+    <>
+      <header>
+        <h1>JAMMING</h1>
+      </header>
+      <main>
+        <section>
+          <form>
+            <SearchResult />
+            <button type="button">Search</button>
+          </form>
+        </section>
+        <section>
+          <div>Result</div>
+          <div>Playlist</div>
+        </section>
+      </main>
+      <footer>
+        <p>&copy;Codecademy Project</p>
+      </footer>
+    </>
+  );
 }
 
-export default App
+export default App;

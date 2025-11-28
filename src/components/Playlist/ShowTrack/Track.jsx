12efe11ea3b9fbdd224709e7d288275c26f1a2a5
SoraK93 <176456696+SoraK93@users.noreply.github.com>
2025-11-28 01:35:23 +0000

    Add CSS class names to Track component elements for styling

--- src/components/Playlist/ShowTrack/Track.jsx
+++ src/components/Playlist/ShowTrack/Track.jsx
@@ -2,13 +2,13 @@ function Track({ tracks, onClick }) {
   const trackList = tracks.map((song) => {
     return (
       <li key={song.id}>
-        <div className="">
+        <div className="songDetail">
           <h3>{song.name}</h3>
           <p>
             {song.artist} | {song.album}
           </p>
         </div>
-        <div>
+        <div className="addButton">
           <button type="button" id={song.id} onClick={onClick}>
             +
           </button>
